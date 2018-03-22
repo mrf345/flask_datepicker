15fa1fe56022f80f2f0dc957b0c74304f70975c3
mrf3 <mrf345@gmail.com>
2018-03-22 17:04:23 +0800

    adding 0.3 rel

--- setup.py
+++ setup.py
@@ -13,9 +13,9 @@ from setuptools import setup
 
 setup(
     name='Flask-Datepicker',
-    version='0.2',
+    version='0.3',
     url='https://github.com/mrf345/flask_datepicker/',
-    download_url='https://github.com/mrf345/flask_datepicker/archive/0.2.tar.gz',
+    download_url='https://github.com/mrf345/flask_datepicker/archive/0.3.tar.gz',
     license='MIT',
     author='Mohamed Feddad',
     author_email='mrf345@gmail.com',
