65c17b37d9be7b023efda1adc9f3b7cdafcf6b75
Mohamed Feddad <mrf345@gmail.com>
2024-07-22 23:18:18 +0400

    chore: uppercase class name

--- flask_datepicker/__init__.py
+++ flask_datepicker/__init__.py
@@ -1,2 +1,3 @@
-from flask_datepicker.main import datepicker  # noqa
+from flask_datepicker.main import Datepicker  # noqa
+from flask_datepicker.main import Datepicker as datepicker  # noqa
 from flask_datepicker.about import (__license__, __version__, __author__, __doc__, __email__) # noqa
